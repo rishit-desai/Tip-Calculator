8d344d5c9f957c19a007743b569d0745d7322cd2
Rishit Desai <rishit-desai@users.noreply.github.com>
2021-07-25 16:30:17 +0530

    improvement to handle edge cases

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById("total-tip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
-document.getElementById("submit").addEventListener('click', calculateTip );
+document.getElementById("submit").addEventListener('click', calculateTip);
 
-document.addEventListener("keypress",function(e)
+document.addEventListener("keypress", function (e)
 {
-    if(e.keyCode == 13)
+    if (e.keyCode == 13)
     {
         calculateTip();
     }
@@ -13,20 +13,31 @@ document.addEventListener("keypress",function(e)
 
 function calculateTip() 
 {
-    var bill, people,percentage;
+    var bill, people, percentage;
     bill = document.getElementById("bill-input").value;
-    people= document.getElementById("people-number").value;
+    people = document.getElementById("people-number").value;
     percentage = document.getElementById("Options").value;
 
-    if(bill === "" || percentage === 0)
+    if (bill === "" || percentage === 0)
     {
         window.alert("Enter some values");
         return;//prevent function from continuing
     }
-
-    if(people === "" || people <=1)
+    else if (isNaN(bill))
     {
-        people=1;
+        window.alert("Please enter bill amount");
+        return;
+    }
+
+    if (isNaN(people))
+    {
+        window.alert('Please enter number of people')
+        return;
+    }
+
+    if (people === "" || people <= 1)
+    {
+        people = 1;
         document.getElementById("each").style.display = "none";
     }
     else
@@ -40,6 +51,6 @@ function calculateTip()
     document.getElementById("total-tip").style.display = "block";
     document.getElementById("tip").innerHTML = tip;
 
-    window.scrollBy(0,50);
+    window.scrollBy(0, 50);
 }
 
